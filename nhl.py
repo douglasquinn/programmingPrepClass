43613ccf7d4f30b473188c093e59487e18283e23
douglas.quinn <Douglas.quinn3827@gmail.com>
2021-09-27 12:28:34 -0400

    Add files via upload

---
+++ nhl.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+excelFile = pd.ExcelFile('/Users/douglasquinn/Desktop/nhl_skaterstats.xlsx')
+players = pd.read_excel(excelFile, 'players')
+stats = pd.read_excel(excelFile, 'stats')
+teams = pd.read_excel(excelFile, 'teams')
+positions = pd.read_excel(excelFile, 'positions')
+print('Excel sheets imported')
+
+players.info()
